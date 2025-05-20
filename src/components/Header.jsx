545effad97526b7aab369c1bcb4574f4c066bff5
Nicolapsq <nicolapasqua84@gmail.com>
2025-05-20 17:07:52 +0200

    Add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,27 @@
 export default function Header() {
-    return <h1>Header</h1>
+  return (
+    <div className="container">
+      <nav className="navbar">
+        <div>
+            <img src="public\dc-logo.png" alt="DC-logo" />
+        </div>
+        <div>
+            <ul>
+                <li>CHARACTERS</li>
+                <div className="border-active">
+                <li className="active" >COMICS</li>
+                </div>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </div>
+      </nav>
+    </div>
+  );
 }
