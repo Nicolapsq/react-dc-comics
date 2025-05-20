26df914cdc31e4b1a8f0115ca8f87daf60f0ce1c
Nicolapsq <nicolapasqua84@gmail.com>
2025-05-20 19:20:02 +0200

    Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,88 @@
 export default function Footer() {
-    return <h1>Footer</h1>
+    return (
+        <div className="bg-image">
+            <div className="container">
+                <div className="flex space-between">
+                    <div className="flex">
+                        <div className="list-comics-shop">
+                            <div>
+                                <div className="title-list">DC COMICS</div>
+                                <div className="list">DC COMICS</div>
+                                <div className="list">DC COMICS</div>
+                                <div className="list">DC COMICS</div>
+                                <div className="list">DC COMICS</div>
+                                <div className="list">DC COMICS</div>
+                                <div className="list">DC COMICS</div>
+                                <div className="list">DC COMICS</div>
+                            </div>
+                            <div>
+                                <div className="title-list">SHOP</div>
+                                <div className="list">DC COMICS</div>
+                                <div className="list">DC COMICSsssssssssssssssssssssss</div>
+                            </div>
+                        </div>
+                        <div className="flex list-dc-sites">
+                            <div>
+                                <div className="title-list">DC</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+                            </div>
+                            <div>
+                                <div className="title-list">SITES</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+                                <div className="list">dc comics</div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="public\dc-logo-bg.png" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className="bg-color-grey">
+                <div className="container">
+                    <div className="flex space-between">
+                        <div>
+                            <button>
+                                SING-UP NOW
+                            </button>
+                        </div>
+                        <div>
+                            <ul className="flex justify-content-c align-items-c">
+                                <li>FOLLOW US</li>
+                                <li>
+                                    <img src="public\footer-facebook.png" alt="facebook" />
+                                </li>
+                                 <li>
+                                    <img src="public\footer-twitter.png" alt="twitter" />
+                                </li>
+                                 <li>
+                                    <img src="public\footer-youtube.png" alt="you-tube" />
+                                </li>
+                                 <li>
+                                    <img src="public\footer-pinterest.png" alt="pinterest" />
+                                </li>
+                                 <li>
+                                    <img src="public\footer-periscope.png" alt="periscope" />
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    );
 }
